9592da29ae31cc2cff514116681ff7d20c37ed0e
rashidulub <rashidul016094@gmail.com>
2023-05-02 13:53:45 +0600

    chef card component create

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLoaderData, useParams } from 'react-router-dom';
+import ChefCard from '../ChefCard/ChefCard';
 
 const Category = () => {
     const {id} = useParams();
+    const categoriChef = useLoaderData()
     return (
         <div>
-            <h2>this is a Category: {id}</h2>
+            <h2>this is a Category: {categoriChef.length}</h2>
+            {
+                categoriChef.map(chef=>
+                    <ChefCard
+                    key={chef._id}
+                    chef={chef}
+                    ></ChefCard>)
+            }
         </div>
     );
 };
---
+++ src/pages/Home/ChefCard/ChefCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChefCard = () => {
+    return (
+        <div>
+            <h1>chef card </h1>
+        </div>
+    );
+};
+
+export default ChefCard;
