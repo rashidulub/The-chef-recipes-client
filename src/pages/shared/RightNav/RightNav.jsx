75bb08128254c435b799a7f9d0071eea286c5099
rashidulub <rashidul016094@gmail.com>
2023-05-04 00:58:55 +0600

    login with github added

--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -3,13 +3,15 @@ import { Button, ListGroup } from 'react-bootstrap';
 import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaInstagram, FaDiscord } from 'react-icons/fa';
 import FoodZone from '../FoodZone/FoodZone';
 import { AuthContext } from '../../../Provider/AuthProvider';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import app from '../../../firebase/firebase.config';
 
 const RightNav = () => {
     const auth = getAuth(app);
 
     const provider = new GoogleAuthProvider();
+
+    const GithubProvider = new GithubAuthProvider()
     
     
     const handleGoogleSignIN =()=>{
@@ -21,12 +23,21 @@ const RightNav = () => {
         .catch(error=>console.log(error))
     }
 
+    const hendleGithubSingIn =()=>{
+        signInWithPopup(auth,GithubProvider)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch(error=>console.log(error))
+    }
+
     return (
         <div className='text-center'>
 
             <h4 className='mb-3 text-center mt-3 ' >Login With </h4>
             <Button onClick={handleGoogleSignIN} className='mb-2 ' variant="outline-primary"><FaGoogle /> Login with Google</Button>
-            <Button variant="outline-secondary"> <FaGithub className='mr-3' />Login with Github  </Button>
+            <Button onClick={hendleGithubSingIn} variant="outline-secondary"> <FaGithub className='mr-3' />Login with Github  </Button>
             <div className='mt-4'>
                 <h4>Find Us On</h4>
                 <ListGroup>
